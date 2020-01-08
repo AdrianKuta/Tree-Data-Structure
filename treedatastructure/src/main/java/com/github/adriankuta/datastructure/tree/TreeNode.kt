97b350281273f2f011672c26f0e94538d63ac23e
Adrian Kuta <adrian.kuta93@gmail.com>
2020-01-08 13:24:24 +0100

    Tree as android library.

---
+++ treedatastructure/src/main/java/com/github/adriankuta/datastructure/tree/TreeNode.kt
@@ -0,0 +1,67 @@
+package com.github.adriankuta.datastructure.tree
+
+class TreeNode<T>(val value: T) {
+
+    private var parent: TreeNode<T>? = null
+    private val children = mutableListOf<TreeNode<T>>()
+
+    fun addChild(child: TreeNode<T>) {
+        child.parent = this
+        children += child
+    }
+
+    fun removeChild(child: TreeNode<T>): Boolean {
+        if (children.isEmpty()) {
+            return false
+        }
+
+        val nestedChildRemoved = children.map {
+            it.removeChild(child)
+        }.reduce { acc, b -> acc or b }
+
+        return children.remove(child) or nestedChildRemoved
+    }
+
+    fun getParent(): TreeNode<T>? = parent
+
+    fun getChildren(): List<TreeNode<T>> = children
+
+    fun size(): Int {
+        if (children.isEmpty())
+            return 0
+        return children.size +
+                children.sumBy { it.size() }
+
+    }
+
+    fun depth(): Int {
+
+        val childrenMaxDepth = children.map { it.depth() }.max() ?: 0
+        return childrenMaxDepth + 1
+    }
+
+    fun allItems(): List<T> {
+        return children.map { value }
+    }
+
+    override fun toString(): String {
+        val stringBuilder = StringBuilder()
+        print(stringBuilder, "", "")
+        return stringBuilder.toString()
+    }
+
+    private fun print(stringBuilder: StringBuilder, prefix: String, childrenPrefix: String) {
+        stringBuilder.append(prefix)
+        stringBuilder.append(value)
+        stringBuilder.append('\n')
+        val childIterator = children.iterator()
+        while (childIterator.hasNext()) {
+            val node = childIterator.next()
+            if(childIterator.hasNext()) {
+                node.print(stringBuilder, "$childrenPrefix├── ", "$childrenPrefix│   ")
+            } else {
+                node.print(stringBuilder, "$childrenPrefix└── ", "$childrenPrefix    ")
+            }
+        }
+    }
+}
