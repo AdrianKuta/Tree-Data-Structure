38c3c268c033ced3ed71e2940f2f82acaae92e42
Adrian Kuta <adrian.kuta93@gmail.com>
2020-01-11 00:43:41 +0100

    Removed unnecessary method

--- treedatastructure/src/main/java/com/github/adriankuta/datastructure/tree/Temp.java
+++
@@ -1,9 +0,0 @@
-package com.github.adriankuta.datastructure.tree;
-
-public class Temp {
-
-    public static void main(String[] args) {
-        TreeNode<String> temp = new TreeNode<>("Test");
-
-    }
-}
