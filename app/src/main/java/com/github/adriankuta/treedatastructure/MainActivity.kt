784ab07442d738653bef0495f95f6436cf6401fe
Adrian Kuta <adrian.kuta93@gmail.com>
2021-09-03 11:17:36 +0200

    feat: Upgrade build gradle to 7.0.2

--- app/src/main/java/com/github/adriankuta/treedatastructure/MainActivity.kt
+++ app/src/main/java/com/github/adriankuta/treedatastructure/MainActivity.kt
@@ -2,11 +2,28 @@ package com.github.adriankuta.treedatastructure
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.github.adriankuta.datastructure.tree.tree
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val root =
+            tree("World") {
+                child("North America") {
+                    child("USA") {
+                        child("LA")
+                        child("New York")
+                    }
+                }
+                child("Europe") {
+                    child("Poland")
+                    child("Germany")
+                }
+            }
+        Log.d("DEBUG_TAG", root.prettyString())
     }
 }
